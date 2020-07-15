a4651ac6eb6e9db2b5ea50f086a295efcf40cca8
Freddy875 <fer875nandosm@gmail.com>
2020-07-14 19:31:36 -0500

    Estamos aprendiendo Batch

---
+++ HolaMundo.bat
@@ -0,0 +1,7 @@
+@echo off
+title Mi Primer Programa en Batch
+echo ---------------------------------------
+echo Hola Mundo Bienvenido al mundo de Batch
+echo ---------------------------------------
+pause > nul
+exit
